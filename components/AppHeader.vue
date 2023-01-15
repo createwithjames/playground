0417fa3eac8d14ed3dd04611661d7b51532ac26f
James Phillips <james@moresoda.co.uk>
2023-01-15 23:05:03 +0000

    Started basic nav and layout

---
+++ components/AppHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="stickyw-full border-b border-slate-900/10">
+    <div class="mx-auto max-w-7xl">
+      <div class="py-4 px-8">
+        <div class="flex items-center justify-between">
+          <div class="flex-none">
+            <NuxtLink to="/" class="flex gap-1">
+              <BeakerIcon class="w-6 text-orange-500" />
+              <span>Playground</span>
+            </NuxtLink>
+          </div>
+          <nav>
+            <ul class="flex gap-4">
+              <li>
+                <NuxtLink to="/calculator">Calculator</NuxtLink>
+              </li>
+              <li>Calculator</li>
+              <li>Calculator</li>
+              <li>Calculator</li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script setup>
+import { BeakerIcon } from "@heroicons/vue/24/outline";
+</script>
